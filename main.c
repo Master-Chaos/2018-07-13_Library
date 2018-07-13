2790104eda08a81e18f2f0eb6981e1668571bf5e
Master-Chaos <thomas_pointner@gmx.at>
2018-07-13 16:29:07 +0200

    add_book finisched

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ typedef struct books
     char  author[MAXLENGTH];
     int   isbn;
     struct books* next;
+    int bookcount;
 } book;
 
 void init_books(book *books)
@@ -17,6 +18,7 @@ void init_books(book *books)
     bzero(books->author, MAXLENGTH);
     books->isbn = 0;
     books->next = NULL;
+    books->bookcount = 0;
 
 }
 
@@ -29,27 +31,35 @@ void print_bookall(book *books)
 
 }
 
-int add_book(book *books, char title[MAXLENGTH], char  author[MAXLENGTH], int isbn, struct books* next)
+int add_book(book *books, char title[MAXLENGTH], char  author[MAXLENGTH], int isbn)
 {
-    strcpy(books->title, title);
-    strcpy(books->author, author);
-    books->isbn = isbn;
-    books->next = books;
+    book* last = books;
+    book* new;
+    new = malloc(sizeof(books));
+
+    while(last->next != NULL)
+    {
+        last = last->next;
+    }
+    last->next = new;
+    strcpy(new->title, title);
+    strcpy(new->author, author);
+    new->isbn = isbn;
+    new->next = NULL;
+    books->bookcount++;
 
     return 0;
 }
 
 int main()
 {
-    book book1;
-    book book2;
-    init_books(&book1);
-    init_books(&book2);
-    add_book(&book1,"Am Ende steht Gott", "Jesus Christus", 123456, &book2);
+    book books;
+    init_books(&books);
+    add_book(&books,"Am Ende steht Gott", "Jesus Christus", 123456, &book2);
 
 
 
     printf("Hello world!\n");
-    print_bookall(&book1);
+    print_bookall(&books);
     return 0;
 }
