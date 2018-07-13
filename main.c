5bd08ad4414808461a1a17581939defd3809789b
Master-Chaos <thomas_pointner@gmx.at>
2018-07-13 15:03:27 +0200

    Programm beginnen

--- main.c
+++ main.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAXLENGTH 50
 
-int main() {
-    printf("Hello, World!\n");
+typedef struct books
+{
+    char  title[MAXLENGTH];
+    char  author[MAXLENGTH];
+    int   isbn;
+    struct books* next;
+} book;
+
+void init_books(book *books)
+{
+    bzero(books->title, MAXLENGTH);
+    bzero(books->author, MAXLENGTH);
+    books->isbn = 0;
+    books->next = NULL;
+
+}
+
+void print_bookall(book *books)
+{
+    printf("Buchtitel: %s\n", books->title);
+
+}
+
+int add_book(book *books, char title[MAXLENGTH], char  author[MAXLENGTH], int isbn, struct books* next)
+{
+
+    return 0;
+}
+
+int main()
+{
+    book book1;
+    init_books (&book1);
+
+
+
+
+    printf("Hello world!\n");
     return 0;
 }
